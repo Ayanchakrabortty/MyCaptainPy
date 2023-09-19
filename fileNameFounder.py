dbe6e2de4ec89b6bc1be5205b30f9ef813f00772
Ayanchakrabortty <72329605+Ayanchakrabortty@users.noreply.github.com>
2023-09-19 12:17:59 +0530

    Update fileNameFounder.py

--- fileNameFounder.py
+++ fileNameFounder.py
@@ -1,8 +1,6 @@
-# Input the filename from the user
-filename = input("Enter a filename: ")
 
-# Split the filename to get the file extension
+filename = input("Enter a filename: ")
 file_extension = filename.split(".")[-1]
 
-# Print the file extension without the dot
+
 print(f"The file extension is: {file_extension}")
